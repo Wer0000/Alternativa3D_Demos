c911dbf2d2534297900b6a80918a562adc3b4688
Wer0000 <60045423+Wer0000@users.noreply.github.com>
2022-11-19 12:48:42 +0300

    Add files via upload

---
+++ alternativa/arena/integrator.js
@@ -0,0 +1,16 @@
+processGoogleToken({newToken: "", validLifetimeSecs: 300, freshLifetimeSecs: 300, 1p_jar: "2022-11-19-08", pucrd: ""});
+1p_jar
+:
+"2022-11-19-08"
+freshLifetimeSecs
+:
+300
+newToken
+:
+""
+pucrd
+:
+""
+validLifetimeSecs
+:
+300
